dc889b28490b9934f61209822d655d90db0b8460
Felipe Soller <soller@outlook.com>
2020-06-14 21:14:17 -0300

    Creating the BusinessList Component

--- src/components/Business/Business.js
+++ src/components/Business/Business.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import './Business.css';
 
 const business = {
     imageSrc: 'https://s3.amazonaws.com/codecademy-content/programs/react/ravenous/pizza.jpg',
@@ -14,17 +15,17 @@ const business = {
 
 class Business extends React.Component {
     render() {
-        return {
+        return (
             <div className="Business">
                 <div className="image-container">
                     <img src='https://s3.amazonaws.com/codecademy-content/programs/react/ravenous/pizza.jpg' alt=''/>
                 </div>
-            <h2>{business.name}</h2>
+                <h2>{business.name}</h2>
                 <div className="Business-information">
                     <div className="Business-address">
                         <p>{business.address}</p>
                         <p>{business.city}</p>
-                        <p>{business.state} {business.zipCode}/p>
+                        <p>{business.state} {business.zipCode}</p>
                     </div>
                     <div className="Business-reviews">
                         <h3>{business.category}</h3>
@@ -33,6 +34,8 @@ class Business extends React.Component {
                     </div>
                 </div>
             </div>
-        }
+        )
     }
 }
+
+export default Business;
