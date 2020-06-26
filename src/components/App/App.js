1cc564bf5cf03acd0ddb7fd8f95107c240f53475
Felipe Soller <soller@outlook.com>
2020-06-26 17:08:31 -0300

    Passed information from parent components (App) to child components (BusinessList, Business)

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,8 +1,30 @@
 import React from 'react';
-//import logo from './logo.svg';
 import './App.css';
-import SearchBar from '../SearchBar/SearchBar';
+//import logo from './logo.svg';
 import BusinessList from '../BusinessList/BusinessList';
+import SearchBar from '../SearchBar/SearchBar';
+
+
+const business = {
+  imageSrc: 'https://s3.amazonaws.com/codecademy-content/programs/react/ravenous/pizza.jpg',
+  name: 'Luca Pizzeria',
+  address: '1010 Paddington Way',
+  city: 'Flavortown',
+  state: 'NY',
+  zipCode: '10101',
+  category: 'Italian',
+  rating: 4.5,
+  reviewCount: 90
+};
+
+const businesses = [
+  business, 
+  business, 
+  business, 
+  business, 
+  business, 
+  business
+];
 
 class App extends React.Component {
   render() {
@@ -10,7 +32,7 @@ class App extends React.Component {
       <div className="App">
         <h1>ravenous</h1>
         <SearchBar />
-        <BusinessList />
+        <BusinessList businesses={businesses}/>
       </div>
     );
   }
--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -28,7 +28,7 @@ class SearchBar extends React.Component {
                     <input placeholder="Where?" />
                 </div>
                 <div className="SearchBar-submit">
-                    <a>Let's Go</a>
+                    <a href="/#">Let's Go</a>
                 </div>
             </div>
         )
