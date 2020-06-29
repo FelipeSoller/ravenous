f8ab96cf8588dcc504a8ec2f2eb4d5d6ba310e44
Felipe Soller <soller@outlook.com>
2020-06-28 23:45:35 -0300

    Bind Methods in the Constructor

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -15,6 +15,8 @@ class SearchBar extends React.Component {
             location: '',
             sortBy: 'best_match' 
         }
+        this.handleTermChange = this.handleTermChange.bind(this)
+        this.handleLocationChange = this.handleLocationChange.bind(this)
     }
 
     getSortByClass(sortByOption) {
@@ -29,11 +31,11 @@ class SearchBar extends React.Component {
     }
 
     handleTermChange(event) {
-        this.setState({ })
+        this.setState({ term: event.target.value })
     }
 
     handleLocationChange(event) {
-        this.setState({ })
+        this.setState({ location: event.target.value })
     }
 
     renderSortByOptions() {
