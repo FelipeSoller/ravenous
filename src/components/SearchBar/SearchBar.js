26201b7ee78b912c9d3c1a2f384298c1465c7c82
Felipe Soller <soller@outlook.com>
2020-06-28 23:34:00 -0300

    Set the Class Name of a Sort Option

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -31,7 +31,7 @@ class SearchBar extends React.Component {
     renderSortByOptions() {
         return Object.keys(sortByOptions).map(sortByOption => {
             let sortByOptionValue = sortByOptions[sortByOption];
-            return <li key={sortByOptionValue}>{sortByOption}</li>;
+            return <li key={sortByOptionValue} className={this.getSortByClass(sortByOptionValue)}>{sortByOption}</li>;
         });
     }
 
